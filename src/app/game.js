b8e2f4b614a07ea79416a4378a0c7897751895d9
Filipe Stefano Ghisi <filipe.ghisi@gmail.com>
2021-06-06 22:14:19 -0300

    Eslint.

--- src/app/game.js
+++ src/app/game.js
@@ -1,23 +1,17 @@
-import * as PIXI from "pixi.js";
-import * as PIXI3D from "pixi3d";
+import * as PIXI from 'pixi.js';
+import * as PIXI3D from 'pixi3d';
 
-export default function (parent = window) {
-  let app = new PIXI.Application({
-    backgroundColor: 0xdddddd,
-    resizeTo: parent,
-    antialias: true,
-  });
+export default function game(parent = window) {
+  const app = new PIXI.Application({ backgroundColor: 0xdddddd, resizeTo: parent, antialias: true });
 
-  let mesh = app.stage.addChild(PIXI3D.Mesh3D.createCube());
+  const mesh = app.stage.addChild(PIXI3D.Mesh3D.createCube());
 
   PIXI3D.LightingEnvironment.main.lights.push(
-    Object.assign(new PIXI3D.Light(), { x: -1, z: 3 })
+    Object.assign(new PIXI3D.Light(), { x: -1, z: 3 }),
   );
 
   let rotation = 0;
-  app.ticker.add(() => {
-    mesh.rotationQuaternion.setEulerAngles(0, rotation++, 0);
-  });
+  app.ticker.add(() => mesh.rotationQuaternion.setEulerAngles(0, rotation++, 0));
 
   return app;
 }
