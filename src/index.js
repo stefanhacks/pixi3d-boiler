966f027c048a2ef56334df0d03c2158be7359a14
Filipe Stefano Ghisi <filipe.ghisi@gmail.com>
2021-06-06 22:48:33 -0300

    Husky adjustments.

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ const resize = () => {
 
 function make() {
   document.body.style.display = 'flex';
+  document.body.style.margin = 0;
   document.body.style.alignItems = 'center';
   document.body.style.justifyContent = 'center';
-  document.body.style.margin = 0;
   document.body.style.backgroundColor = 'black';
   window.addEventListener('resize', () => resize());
 
